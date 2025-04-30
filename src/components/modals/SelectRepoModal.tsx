ad9431983aab5f246fa39b8e1d1bba684c28fc4a
arrismo <mikaelmoise00@gmail.com>
2025-04-29 21:46:13 -0400

    adding console log messages

--- src/components/modals/SelectRepoModal.tsx
+++ src/components/modals/SelectRepoModal.tsx
@@ -14,6 +14,7 @@ const SelectRepoModal: React.FC<SelectRepoModalProps> = ({ isOpen, onClose, onCr
   const [searchTerm, setSearchTerm] = useState('');
   
   useEffect(() => {
+    console.log('SelectRepoModal useEffect triggered. isOpen:', isOpen);
     if (isOpen) {
       fetchRepositories();
     }
